fd6d3bfb509d60a700b2589981dd199f8c8bf970
Simon Ferns <delaywasntaken@gmail.com>
2023-08-11 05:45:01 +0200

    Added correct Keys

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,4 @@
-export const RAPIDAPI_KEY = '9068acfd34msh47e2c92a2df8fa3p171f4ejsnd86e2e8faa1d'
+export const RAPIDAPI_KEY = 'b198a1761fmsh47f0bc0beb98915p1cdf6fjsn59be3888c67b'
 export const APIFY_TOKEN = 'KVlnQbUMyoSouH3ir2zpCmVEHoUwTpnKfXbhjeYxtuC1C8uUtCxOSqavK6CAMWmlIHH9M91ghDJrVGnbwa8cYZbIY1DDJCtGJNw0E6kDVZLt845H4PwQCWlu6DjUvElC'
 export const APIFY_WORKER_URL = ''
 
